b2b5cd3acadab5e5ad392aa6b00e948ccb064431
unknown <marcivan98@gmail.com>
2020-08-19 11:33:50 -0400

    Issue 5: fixed typo

--- rook.py
+++ rook.py
@@ -2,7 +2,7 @@ from chessPiece import ChessPiece
 
 
 class Rook(ChessPiece):
-    """docstring for Queen"""
+    """docstring for Rook"""
 
     def __init__(self, color, x, y):
         super(Rook, self).__init__(color, x, y)

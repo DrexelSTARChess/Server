a4b18c24f7565da6dd31251f45acee4cde1f6f6a
suspectful <gcontractor17@gmail.com>
2020-08-20 15:35:36 -0400

    Implemented back-and-forth in server, added player and helper class

---
+++ helper.py
@@ -0,0 +1,7 @@
+color_to_player_num = {"white": 1, "black": 2}
+player_num_to_color = {1: "white", 2: "black"}
+def get_other_player_num(player_number):
+    if player_number == 1:
+        return 2
+    else:
+        return 1
--- server.py
+++ server.py
@@ -1,13 +1,35 @@
-from flask import Flask, escape, request
-import ujson
-import gameHandler
+from flask import Flask, escape, request, jsonify
+import time
+
+from board import *
+from boardTranslator import clientToServer, serverToClient
+from helper import player_num_to_color, get_other_player_num
+from player import Player
 
 app = Flask(__name__)
+player_count = 0
+board = None
+players = {1: None, 2: None}
 
 
 @app.route('/startGame')
 def start_game():
-	return
+	global player_count
+	global board
+
+	# Server is full, cannot accept new players
+	if player_count >= 2:
+		return jsonify({"status_code": 503})
+
+	# Player has connected, board created
+	player_count += 1
+	if player_count == 1:
+		players[1] = Player("white")
+		board = Board()
+	else:
+		players[2] = Player("black")
+
+	return jsonify({"status_code": 200, "player": player_count, "color": players[player_count].color})
 
 
 @app.route('/quitGame')
@@ -15,11 +37,90 @@ def quit_game():
 	return
 
 
+@app.route('/waitForPlayer')
+def wait_for_player():
+	global player_count
+	arguments = request.get_json()
+	player_number = arguments["player_number"]
+
+	while player_count < 2:
+		time.sleep(0.5)
+
+	return jsonify(
+				{
+					"status_code": 200,
+					"boardData": serverToClient(board),
+					"moveData": board.generateMoves(player_num_to_color[player_number])
+				}
+			)
+
+
 @app.route('/waitForTurn')
 def wait_for_turn():
-	return
+	global board
+
+	arguments = request.get_json()
+	player_number = arguments["player_number"]
+	current_player = players[player_number]
+	# Check if current_player has won
+	if current_player.has_won:
+		return jsonify(
+			{
+				"status_code": 200,
+				"board_data": serverToClient(board),
+				"move_data": [],
+				"won": True,
+				"lost": False
+			}
+		)
+
+	# Wait other player's turn to end
+	while current_player.is_turn == False:
+		time.sleep(0.5)
+
+	# If game is over, then this player has lost
+	moves = board.generateMoves(current_player.color)
+	if moves is []:
+		current_player.has_lost = True
+		players[get_other_player_num(player_number)].has_won = True
+		return jsonify(
+			{
+				"status_code": 200,
+				"board_data": serverToClient(board),
+				"move_data": [],
+				"won": False,
+				"lost": True,
+			}
+		)
+
+	# Nobody has won, so continue the game
+	return jsonify(
+		{
+			"status_code": 200,
+			"board_data": serverToClient(board),
+			"move_data": moves,
+			"won": False,
+			"lost": False
+		}
+	)
 
 
 @app.route('/submitBoard')
 def submit_board():
-	return
+	global board
+	# We need to let wait_for_turn know to move on
+
+	arguments = request.get_json()
+	player_number = arguments["player"]
+	board.setBoard(clientToServer(arguments["board"]))
+
+	# Switching players
+	players[player_number].is_turn = False
+	other_player_number = get_other_player_num(player_number)
+	players[other_player_number].is_turn = True
+
+	return jsonify(
+				{
+					"status_code": 200,
+				}
+			)

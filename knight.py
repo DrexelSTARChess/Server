e513cb7b3c35f4df727d1db32c9aaa1fef296665
mygenel <myg26@drexel.edu>
2020-08-22 18:56:25 -0400

    utilized proper inheritance for knight

--- knight.py
+++ knight.py
@@ -10,4 +10,79 @@ class Knight(ChessPiece):
         self.name = "Knight"
 
     def getPossibleMoves(self, board):
-        return self.getKnightMoves(board)
+        possibleMoves = []
+        movei = self.i + 2
+        movej = self.j + 1
+        if self.isBoundedSquare(movei, movej):
+            if board[movei][movej] == "noPiece":
+                move = "%d,%d" % (movei, movej)
+                possibleMoves.append(move)
+            elif board[movei][movej].color != self.color:
+                move = "x%d,%d" % (movei, movej)
+                possibleMoves.append(move)
+        movei = self.i + 2
+        movej = self.j - 1
+        if self.isBoundedSquare(movei, movej):
+            if board[movei][movej] == "noPiece":
+                move = "%d,%d" % (movei, movej)
+                possibleMoves.append(move)
+            elif board[movei][movej].color != self.color:
+                move = "x%d,%d" % (movei, movej)
+                possibleMoves.append(move)
+        movei = self.i - 2
+        movej = self.j + 1
+        if self.isBoundedSquare(movei, movej):
+            if board[movei][movej] == "noPiece":
+                move = "%d,%d" % (movei, movej)
+                possibleMoves.append(move)
+            elif board[movei][movej].color != self.color:
+                move = "x%d,%d" % (movei, movej)
+                possibleMoves.append(move)
+        movei = self.i - 2
+        movej = self.j - 1
+        if self.isBoundedSquare(movei, movej):
+            if board[movei][movej] == "noPiece":
+                move = "%d,%d" % (movei, movej)
+                possibleMoves.append(move)
+            elif board[movei][movej].color != self.color:
+                move = "x%d,%d" % (movei, movej)
+                possibleMoves.append(move)
+        # restart
+        movei = self.i + 1
+        movej = self.j + 2
+        if self.isBoundedSquare(movei, movej):
+            if board[movei][movej] == "noPiece":
+                move = "%d,%d" % (movei, movej)
+                possibleMoves.append(move)
+            elif board[movei][movej].color != self.color:
+                move = "x%d,%d" % (movei, movej)
+                possibleMoves.append(move)
+        movei = self.i + 1
+        movej = self.j - 2
+        if self.isBoundedSquare(movei, movej):
+            if board[movei][movej] == "noPiece":
+                move = "%d,%d" % (movei, movej)
+                possibleMoves.append(move)
+            elif board[movei][movej].color != self.color:
+                move = "x%d,%d" % (movei, movej)
+                possibleMoves.append(move)
+        movei = self.i - 1
+        movej = self.j + 2
+        if self.isBoundedSquare(movei, movej):
+            if board[movei][movej] == "noPiece":
+                move = "%d,%d" % (movei, movej)
+                possibleMoves.append(move)
+            elif board[movei][movej].color != self.color:
+                move = "x%d,%d" % (movei, movej)
+                possibleMoves.append(move)
+        movei = self.i - 1
+        movej = self.j - 2
+        if self.isBoundedSquare(movei, movej):
+            if board[movei][movej] == "noPiece":
+                move = "%d,%d" % (movei, movej)
+                possibleMoves.append(move)
+            elif board[movei][movej].color != self.color:
+                move = "x%d,%d" % (movei, movej)
+                possibleMoves.append(move)
+
+        return possibleMoves
